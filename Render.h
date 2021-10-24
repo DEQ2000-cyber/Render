636a062170d1b0fe4d89b4b9711ddfa27b8a9fb1
DEQ2000 <57824140+DEQ2000-cyber@users.noreply.github.com>
2021-10-24 17:22:05 -0300

    Add files via upload

--- Render.h
+++ Render.h
@@ -13,7 +13,7 @@
     MOTOR probado en winer GNU/LINUX
 
 
-    VERSION: Alpha 4.5.0.
+    VERSION: Alpha 4.6.0.
     20:16 Argentina Daniel Efrain Quiroga
     06:20 Mexicam Andres Ruiz perez
     06:36 Colombia Jheison Toro Betancourth
@@ -86,6 +86,9 @@
 
     VERSION: Alpha 4.5.0:
     [SE AGREGO]: cuadricula : POR JTB
+
+    VERSION: Alpha 4.6.0:
+    [SE AGREGO]: lineanorm : POR JTB
 
 */
 
@@ -194,6 +197,7 @@ EXPORT void     FASTCALL polygonfill( uint32_t cantidad, PUNTOS p[], uint32_t co
 EXPORT void     FASTCALL lineagr( int x0, int y0, int x1, int y1, uint32_t colorA, uint32_t colorB ); /* DIBUJA UNA LINEA GRADIENTE CON SUS COORDENADAS ESPECIFICADAS. Y EL COLOR VA DESDE EL COLOR A, ASTA YEGAR AL COLOR B. SE LO LLAMA GRADIENTE DE COLOR */
 EXPORT void     FASTCALL triangulogr( int x, int y, int x1, int y1, int x2, int y2, uint32_t colorA, uint32_t colorB, uint32_t colorC ); /* DIBUJA UN TRIANGULO GRADIENTE CON SUS COORDENADAS ESPECIFICADAS. Y EL COLOR VA DESDE EL COLOR A, LUEGO YEGAR AL COLOR B, LUEGO YEGAR AL COLOR C Y VOLVER AL COLOR DE A, SE LO LLAMA GRADIENTE DE COLOR */
 EXPORT void     FASTCALL cuadricula( int itilesx, int itilesy, uint32_t color ); /* DIBUJA UNA CUADRICULA CON TODO EL RENDER/SCREEN CON SEPARAMIENTOS ESPECIFICADOS. Y COLOR USANDO RGB O RGBA */
+EXPORT void     FASTCALL lineanorm( int x1, int y1, int x2, int y2, uint32_t color ); /* DIBUJA UNA LINEA NORMALIZADA POR VECTORES CON SUS COORDENADAS ESPECIFICADAS. Y COLOR USANDO RGB O RGBA */
 
 /* INPUTS Y OUTPUTS */
 EXPORT _Bool     FASTCALL tecla( char ); /* OBTIENE LAS TECLAS PRESIONADA */
